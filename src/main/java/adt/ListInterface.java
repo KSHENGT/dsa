94957b9d9246d1deb359f0333eac30f7ce2ef5aa
LEEJS-32 <jianshenglee32@gmail.com>
2025-03-31 20:21:36 +0800

    Update ListInterface.java

--- src/main/java/adt/ListInterface.java
+++ src/main/java/adt/ListInterface.java
@@ -8,7 +8,7 @@ public interface ListInterface<T> {
   public boolean add(int newPosition, T newEntry);
 
   public T remove(int givenPosition);
-
+//sdfasf
   public boolean replace(int givenPosition, T newEntry);
 
   public T getEntry(int givenPosition);
